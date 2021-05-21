9d8eff0fbb4593c39acd5df164047bfa0a5d972f
Ceyda Önal <66831721+ceyda11@users.noreply.github.com>
2021-05-21 17:11:38 +0300

    Add files via upload

---
+++ abstractDemo/src/abstractDemo/OracleDatabaseManagerTest.java
@@ -0,0 +1,14 @@
+package abstractDemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OracleDatabaseManagerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ abstractDemo/src/abstractDemo/sqlServerDatabaseManager.java
@@ -0,0 +1,12 @@
+package abstractDemo;
+
+public class sqlServerDatabaseManager extends BaseDatabaseManager{
+
+	@Override
+	public void getData() {
+		System.out.println("Veri getirildi : Sql Server");
+		
+	}
+	
+
+}
